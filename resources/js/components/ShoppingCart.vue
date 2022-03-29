9546435d7de1402bbc73b5608f41675d5f13d95e
Dave Bechtum <dbechtum@gmail.com>
2022-03-29 15:29:57 +0200

    Shopping Cart Updated. Still needs add/remove items functionality.

--- resources/js/components/ShoppingCart.vue
+++ resources/js/components/ShoppingCart.vue
@@ -1,15 +1,17 @@
 <template>
     <div>
-        <i @click="showCart" class="btn btn-light bi bi-basket text-end"></i>
-        <div class="cart p-3">
+        <div class="cart p-3 shadow">
+            <div class="cartCloseButton" @click="closeCart()" style="cursor: pointer;">
+                <p>&#10005;</p>
+            </div>
             <div class="row mb-3" v-for="(item, index) in cart.items" :key="index">
                 <div class="col-md-4">
-                    <img :src="'/public/images/posters/' + item.orientation + '/' + item.id + '.jpg'" width="100%">
+                    <img :src="'/images/posters/' + (item.orientation).toLowerCase() + '/' + item.id + '.jpg'" width="100%">
                 </div>
-                <div class="col-md-3">{{ item.name }}</div>
+                <div class="col-md-3">{{ item.name + " " + item.orientation + " " + item.size }}</div>
                 <div class="col-md-1">{{ item.amount }}</div>
-                <div class="col-md-2">{{ item.price }}</div>
-                <div class="col-md-2">{{ item.totalPrice }}</div>
+                <div class="col-md-2">&euro;{{ item.price }}</div>
+                <div class="col-md-2">&euro;{{ item.totalPrice }}</div>
             </div>
 
             <div class="row mt-3">
@@ -17,9 +19,11 @@
                 <div class="col-md-5">Total</div>
                 <div class="col-md-1">{{ cart.totalItems }}</div>
                 <div class="col-md-2"></div>
-                <div class="col-md-2">{{ cart.totalPrice }}</div>
+                <div class="col-md-2">&euro;{{ cart.totalPrice }}</div>
+                <div class="totalDiscount" v-show="cart.totalDiscount>0">You saved &euro;{{ cart.totalDiscount }}</div>
             </div>
         </div>
+        <div class="cartBackground" @click="closeCart()"></div>
     </div>
 </template>
 
@@ -37,16 +41,21 @@
 
         methods: {
             closeCart() {
-                $('.layer').fadeOut(); 
+                $('.cartBackground').fadeOut(); 
                 $('.cart').fadeOut();
             },
 
             showCart() {
-                $('.layer').fadeIn();
-                $('.cart').toggle({
-                    direction: 'right',
+                $('.cartBackground').fadeIn();
+                $('.cart').fadeIn({
                 });
             },
+        },
+        created() {
+            this.$root.$on('show-cart', function() {
+                this.showCart();
+            }.bind(this));
+            
         }
     }
 </script>
---
+++ resources/js/components/ShoppingCartIcon.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <img class="navCartImg" src="/images/icons/cart.svg" @click="showCart()" width="32" height="32">
+        <span class="cartAmount badge bg-primary" v-if="(cartAmount>0)">{{cartAmount}}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+        data: function () {
+            return {
+                cartAmount: 0,
+            };
+        },
+        methods: {
+            showCart() {
+                this.$root.$emit('show-cart');
+            },
+        },
+        created() {
+            this.$root.$on('update-total-items', function(totalItems) {
+
+                this.cartAmount = totalItems;
+
+            }.bind(this));
+        }
+    }
+</script>
